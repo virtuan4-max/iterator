8c20035eaaab367951e19da3d7c087a21294aeca
xRevan <virtuan4@gmail.com>
2026-04-25 18:35:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ rain.loop = true;
 rain.volume = 0;
 randomgods.loop = true;
 
-// --- Document 'n crap ---
+// --- Document 'n functions ---
 
 //ids
 const title = document.getElementById('title');
@@ -21,19 +21,45 @@ const settingsbutton = document.getElementById('settings');
 const introyncheck = document.getElementById('introyncheck');
 const audiotoggle = document.getElementById('audiotoggle');
 const musictoggle = document.getElementById('musictoggle');
+const backcategory = document.getElementById('backcat');
+const pearlselect = document.getElementById('selper');
+const catgselect = document.getElementById('selcat');
+const pearlimagecolor = document.getElementById('pearlcolorinput');
+const crossimagecolor = document.getElementById('crosscolorinput');
+const pearlimage = document.getElementById('pearlimage');
+const nameinput = document.getElementById('pearlnameinput');
+const addbutton = document.getElementById('addbutton');
+const categoryimagecolor = document.getElementById('catcolorinput');
+const categorypreview = document.getElementById('categorypreview');
+
+
 
 // classes
 const settingsmodal = document.querySelector('.settingsmodal');
+const pearlmodal = document.querySelector('.addpearlmodal');
 const maincontent = document.querySelector('.maincontent');
+const selectedpearl = document.querySelector('.selectedpearl');
+const selectedcat = document.querySelector('.selectedcat');
 
-// events   
-document.addEventListener("keydown", handlekeypress);
+
+
+// events  for inputs
 audiotoggle.addEventListener('input', updateVolume);
 musictoggle.addEventListener('input', updateVolume);
+nameinput.addEventListener('input', toggleaddbutton);
+pearlimagecolor.addEventListener('input', updatepearlimagecolors);
+crossimagecolor.addEventListener('input', updatepearlimagecolors);
+categoryimagecolor.addEventListener('input', updatepearlimagecolors);
+
 
 // display
 maincontent.style.display = 'none';
-settingsmodal.style.display = 'none'
+settingsmodal.style.display = 'none';
+pearlmodal.style.display = "none";
+backcategory.style.display = 'none';
+selectedpearl.style.display = "block"
+selectedcat.style.display = "none"
+
 
 // audiobars
 audiotoggle.min = 0;
@@ -42,23 +68,31 @@ musictoggle.min = 0;
 musictoggle.max = 100;
 
 // state 
-let settingsmodalon = false
-let introPlayed = false
-let allpearls = []
-//{
-//category: "Wisdom",
-//pearls: ["Patience", "Clarity"]
-//}
+let settingsmodalon = false;
+let pearlmodalon = false;
+let selectedadd = "pearl";
+let introPlayed = false;
+let allpearls = [];
+let layer = "top";
+let pearlcolor = pearlimagecolor.value;
+let pearlcross = crossimagecolor.value;
+let catglyph = categoryimagecolor.value;
+
+//colors
+let accent = '#C40039';
+let backgroundcolor = '#000000';
+let text = '#FFFFFF';
+let idlemenu = '#A9A4B2';
 
 // localstorage & stuff for settings
 let introplay = localStorage.getItem('introplay')
-if (introplay == null) {
+if (introplay === null) {
     introplay = true
 } else {
     introplay = introplay === 'true';
     introyncheck.style.display = 'none';
 }
-if (introplay == true) {
+if (introplay === true) {
     introyncheck.style.display = 'block';
 }
 
@@ -73,7 +107,19 @@ if (isNaN(musicvol)) musicvol = 1;
 musictoggle.value = musicvol * 100;
 updateVolume()
 
+//clicks for buttons
 document.addEventListener('click', (e) => {
+    //pearls and related
+    const categoryclicked = e.target.closest(".category")
+    if (categoryclicked) {
+        layer = categoryclicked.id
+        updatepearls(layer);
+    }
+    if (e.target.closest('#backcat')) {
+        layer = getupperlayer(layer);
+        updatepearls(layer);
+    }
+
     //buttons making clicky noises
     if (e.target.closest('.circlebutton')) {
         clickhigh.currentTime = 0;
@@ -95,14 +141,47 @@ document.addEventListener('click', (e) => {
         settingsmodal.style.display = 'block';      
     }
     if (e.target.closest('#addpearl')) {
-        addtopearls();
+        pearlmodalon = true;
+        selectedadd = "pearl";
+        maincontent.style.display = 'none';
+        pearlmodal.style.display = 'block';
+        pearlselect.style.filter = "brightness(0) invert(1)";
+        pearlselect.style.zIndex = "4";
     }
+    if (e.target.closest('#selper')) {
+        catgselect.style.removeProperty('filter');
+        catgselect.style.removeProperty("z-index");
+        pearlselect.style.filter = "brightness(0) invert(1)";
+        pearlselect.style.zIndex = "4";
+        selectedadd = "pearl";
+        selectedpearl.style.display = "block"
+        selectedcat.style.display = "none"
+        clearaddpearl();
+
+    }
+    if (e.target.closest('#selcat')) {
+        pearlselect.style.removeProperty('filter');
+        pearlselect.style.removeProperty("z-index");
+        catgselect.style.filter = "brightness(0) invert(1)";
+        catgselect.style.zIndex = "4";
+        selectedadd = "category";
+        selectedpearl.style.display = "none"
+        selectedcat.style.display = "block"
+        clearaddpearl();
+    }
+
     if (e.target.closest('#back')) {
         settingsmodalon = false;
+        pearlmodalon = false;
         maincontent.style.display = 'block';
         settingsmodal.style.display = 'none';
+        pearlmodal.style.display = 'none';
+        setpearlactive();
+        clearaddpearl();
+
     }
     
+    //settigns modal
     if (e.target.closest('#introyn')) {
         if(introplay == true) {
             introplay = false
@@ -115,8 +194,27 @@ document.addEventListener('click', (e) => {
             localStorage.setItem("introplay", introplay);
 
         }
-    }
+    };
 
+    //add pealrs modal
+    if (e.target.closest('#addbutton')) {
+        pearlmodalon = false;
+        pearlmodal.style.display = 'none';
+        maincontent.style.display = 'block';
+        if (selectedadd === "pearl") {
+            addpearl(nameinput.value, layer, pearlcolor, pearlcross);
+            updatepearls(layer);
+        } else {
+            addcategory(nameinput.value, layer, catglyph);
+            updatepearls(layer);
+
+        }
+        setpearlactive();
+        clearaddpearl();
+    };    
+    
+
+    //intro
     if (!introPlayed) {
         introPlayed = true;
         enterclick.play();
@@ -130,16 +228,54 @@ document.addEventListener('click', (e) => {
         else {
             entermaincontent();
         }
-    }
+    };
 });
 
-function handlekeypress(event) {
-    const key = event.key;
-    if (key.toLowerCase() === "s") {
-        rain.pause();
-        entermaincontent();
+//helpers or other functions
+function updatepearlimagecolors() {
+    pearlcolor = pearlimagecolor.value;
+    pearlcross = crossimagecolor.value;
+    catglyph = categoryimagecolor.value;
+    pearlimage.style.setProperty('--color', pearlcolor);
+    pearlimage.style.setProperty('--cross', pearlcross);
+    categorypreview.style.setProperty('--color', catglyph);
+
+}
+function toggleaddbutton() {
+    let input = nameinput.value;
+    if (input === "") {
+        addbutton.style.opacity = "0.5";
+        addbutton.style.pointerEvents = "none";
+        categorypreview.textContent = "D";
+
+    } else {
+        addbutton.style.opacity = "1";
+        addbutton.style.pointerEvents = "auto";
+        categorypreview.textContent = Array.from(input)[0].toUpperCase();
+
     }
 }
+function clearaddpearl() {
+    pearlimagecolor.value = "#929292";
+    crossimagecolor.value = "#FFFFFF";
+    categoryimagecolor.value = accent;
+    categorypreview.textContent = "D";
+    addbutton.style.opacity = "0.5";
+    addbutton.style.pointerEvents = "none";
+
+    nameinput.value = "";
+    updatepearlimagecolors();
+
+}
+function setpearlactive() {
+    catgselect.style.removeProperty('filter');
+    catgselect.style.removeProperty('z-index');
+    pearlselect.style.filter = "brightness(0) invert(1)";
+    pearlselect.style.zIndex = "4";
+    selectedcat.style.display = "none"
+    selectedpearl.style.display = "block"
+}
+
 function fadevolume(audio, from, to, duration, onDone) {
     const steps = 60;
     const interval = duration / steps;
@@ -177,6 +313,8 @@ function updateVolume() {
     });
 }
 
+
+//intro
 function entermaincontent() {
     intro.remove();
     if (maincontent) {
@@ -197,7 +335,6 @@ function entermaincontent() {
     }
     fadevolume(randomgods, 0, 0.09 * musicvol, 3000);
 }
-
 function introSequence() {
     rain.volume= 0;  
     fadevolume(rain, 0,1 * audiovol ,2500, () => {
@@ -219,66 +356,187 @@ function introSequence() {
         }, 3000);
     });
 }
+
+//pearls list
 function findcategory(list, name) {
-  for (const item of list) {
-    if (item.type === "category") {
-      if (item.name === name) return item;
+    for (const item of list) {
+        if (item.type === "category") {
+            if (item.name === name) return item;
 
-      const found = findcategory(item.children, name);
-      if (found) return found;
+            const found = findcategory(item.children, name);
+            if (found) return found;
+        }
     }
-  }
-  return null;
+    return null;
 }
-function addcategory(name, parentcategoryname = null) {
-  const newcategory = {
-    type: "category",
-    name,
-    children: []
-  };
 
-  if (!parentcategoryname) {
-    allpearls.push(newcategory);
-    return;
-  }
+function addcategory(name, categoryname = null, color = accent) {
+    const newcategory = {
+        type: "category",
+        name,
+        color,
+        children: []
+    };
 
-  const parent = findcategory(allpearls, parentcategoryname);
+    if (!categoryname || categoryname === 'top') {
+        allpearls.push(newcategory);
+        return;
+    }
 
-  if (!parent) {
-    console.error("parent category not found");
-    return;
-  }
+    const parent = findcategory(allpearls, categoryname);
 
-  parent.children.push(newcategory);
+    if (!parent) {
+        console.error("parent category not found");
+        return;
+    }
+
+    parent.children.push(newcategory);
 }
-function addpearl(name, categoryname = null) {
-  const newpearl = {
-    type: "pearl",
-    name
-  };
-  if (!categoryname) {
-    allpearls.push(newpearl);
-    return;
-  }
 
-  const category = findcategory(allpearls, categoryname);
+function addpearl(name, categoryname = null, color = '#888', cross = '#fff') {
+    const newpearl = {
+        type: "pearl",
+        name,
+        color,
+        cross,
+    };
 
-  if (!category) {
-    console.error("category not found");
-    return;
-  }
+    if (!categoryname || categoryname === 'top') {
+        allpearls.push(newpearl);
+        return;
+    }
 
-  category.children.push(newpearl);
+    const category = findcategory(allpearls, categoryname);
+
+    if (!category) {
+        console.error("category not found");
+        return;
+    }
+
+    category.children.push(newpearl);
+
 }
-function addtopearls() {
-    //will replace with modal but this is better for rn
-    let pick = prompt("New category or pearl? (enter c for category or p for pearl)")
-    if (pick == "c") {
-        let name = prompt("enter category name");
-        addcategory(name);
+
+function getupperlayer(layername) {
+    if (layername === "top") return "top";
+
+    function search(list, target, parent = null) {
+        for (const item of list) {
+            if (item.type === "category") {
+                if (item.name === target) {
+                    return parent ? parent.name : "top";
+                }
+                const result = search(item.children, target, item);
+                if (result !== null) return result;
+            }
+        }
+        return null;
+    }
+
+    let parent = search(allpearls, layername);
+    if (parent === null) {
+        return "top";
     } else {
-        let name = prompt("enter name");
-        addpearl(name);
+        return parent;
     }
-    console.log(allpearls);
 }
+
+
+
+// --- Orbiting pearls ---
+addcategory("rainworldpearls", null, '#fff')
+addpearl("spearmasterpearl", "rainworldpearls", '#15111a', '#b80000')
+addpearl("orange", "rainworldpearls", '#ff7a02', '#ffc776')
+addpearl("silver", "rainworldpearls", '#b2b2b2', '#e7e7e7')
+addpearl("black", "rainworldpearls", '#191919', '#727272')
+
+//all this is in radians and i lwk prefer degrees but ig its not like that here lol
+//x = centerx + cos(angle) * radius
+//y = centery + sin(angle) * radius
+//r = 0.085 is perfectly in the center of the rings
+//pearls in like lists and functions refers to both pearls and the categories in the layer
+
+const r = 0.15;
+let angles = new Map();
+let speed = 0.001
+
+
+function collectpearls(categoryname = "top") {
+  if (categoryname === "top") {
+    return allpearls;
+  }
+  const category = findcategory(allpearls, categoryname);
+  return category.children;
+}
+
+function renderpearls(pearlsforanimate) {
+  document.querySelectorAll(".pearl, .category").forEach(pearl => {
+    pearl.remove();
+    angles.delete(pearl.id); 
+  });
+  angles.clear()
+
+  pearlsforanimate.forEach((pearl, i) => {
+
+    const div = document.createElement("div");
+
+    if (pearl.type === "category") {
+        div.classList.add("category");
+        div.textContent = [...pearl.name].slice(0, 2).join('').toUpperCase();
+        div.id = pearl.name;
+        div.style.color = pearl.color;
+
+    }
+    if (pearl.type === "pearl") {
+        div.classList.add("pearl");
+        div.id = pearl.name;
+        div.style.setProperty('--color', pearl.color);
+        div.style.setProperty('--cross', pearl.cross);
+
+    }
+
+    maincontent.appendChild(div);
+    angles.set(pearl.name, (2 * Math.PI / pearlsforanimate.length) * i); 
+  });
+}
+
+function animatepearls() {
+    document.querySelectorAll(".pearl, .category").forEach(pearl => {
+        let angle = angles.get(pearl.id);
+        if (angle === undefined) return;
+        angle += speed;
+        angles.set(pearl.id, angle); 
+
+        let centerx = window.innerWidth / 2;
+        let centery = window.innerHeight / 2;
+        let radius = window.innerWidth * r;
+        const x = centerx + Math.cos(angle) * radius; 
+        const y = centery + Math.sin(angle) * radius;
+
+        pearl.style.position = "absolute";
+        pearl.style.left = x + "px";
+        pearl.style.top  = y + "px";
+        pearl.style.transform = "translate(-50%, -50%)";
+    });
+
+    requestAnimationFrame(animatepearls);
+}
+
+function updatepearls(layer) {
+    if (layer === "top") {
+        backcategory.style.display = 'none'
+    } else {
+        backcategory.style.display = 'block'
+    };
+    let pearls = collectpearls(layer); 
+    renderpearls(pearls);
+}
+
+
+
+//all innitialization functions
+updatepearlimagecolors();
+toggleaddbutton();
+
+updatepearls(layer);
+animatepearls();
